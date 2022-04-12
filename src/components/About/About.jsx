c72c81b93f94171cb2ceee9a67e6cbb6c5d842c5
MozkitoKiller <jvbercerqueira@gmail.com>
2022-04-12 13:17:42 -0300

    adicionando logo react em HOME e minha foto no About

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,20 +1,27 @@
 import React from 'react'
 import style from './About.module.css'
-
+import img from '../../assets/img/IMG_2300.jpg'
 
 
 function About() {
+
+
+
+
     return (
         <div className={style.about}>
+            <div className={style.imgs}>
+               <img src={img} alt="" />
+            </div>
             <div className={style.intro}>
                 <h1>Sobre mim</h1>
-                <p>Desenvolvedor front-end sitiado em São Carlos - SP. 
-                    Formado em Gestão e Análise Ambiental em 2019, decidi iniciar minha <br/>
-                    transição de carreira em meados de 2021.<br/>
-                    <br/>
-                    Profissional com perfil analítico, crítico e realista, e com habilidade para o trabalho em equipe.<br/>
-                    Prático, buscando sempre a melhor e mais eficaz/eficiente forma para resolução de problemas <br/>
-                    <br/>
+                <p>Desenvolvedor front-end sitiado em São Carlos - SP.
+                    Formado em Gestão e Análise Ambiental em 2019, decidi iniciar minha <br />
+                    transição de carreira em meados de 2021.<br />
+                    <br />
+                    Profissional com perfil analítico, crítico e realista, e com habilidade para o trabalho em equipe.<br />
+                    Prático, buscando sempre a melhor e mais eficaz/eficiente forma para resolução de problemas <br />
+                    <br />
                     Possuo interesse no desenvolvimento web como um todo, buscando o full-stack.
                 </p>
             </div>
--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import style from './Home.module.css'
-
+import reactjs from '../../assets/img/React.png'
 
 function Home() {
   return (
@@ -10,6 +10,9 @@ function Home() {
         <h1>Olá, <br /> Sou João,<br /> Desenvolvedor Front-end</h1>
         <p>Desenvolvedor Front-end React</p>
       </section>
+      <div className={style.reactjs}>
+        <img src={reactjs} alt="" />
+      </div>
     </div>
   )
 }
